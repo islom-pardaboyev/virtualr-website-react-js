7832ed1ad420e1e0e9933d46d29099809b20827b
Islom Pardaboyev <154039167+islom-pardaboyev@users.noreply.github.com>
2024-06-04 17:38:47 +0500

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import FeatureSection from "./components/FeatureSection";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
+import Pricing from "./components/Pricing";
+import Workflow from "./components/Workflow";
+import Testimionals from "./components/Testimionals";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -9,6 +13,10 @@ const App = () => {
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection />
         <FeatureSection />
+        <Workflow />
+        <Pricing />
+        <Testimionals />
+        <Footer />
       </div>
     </>
   )
